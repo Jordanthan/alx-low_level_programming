021b77750c591c78ddff37fafa39e444547cdd53
Jordanthan <Ogwanathy@gmail.com>
2022-03-22 16:46:38 -0700

    My first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - reset n value to 98
+ * @n: pointer storing address
+ *
+ * Description: Resetting the value of the variable stored at *n address to 98
+ * using a pointer.
+ * Return: nothing.
+ * */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
