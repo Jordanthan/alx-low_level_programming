cc400d01013dd89f964996257982e07190c0add9
Jordanthan <Ogwanathy@gmail.com>
2022-03-22 17:13:43 -0700

    My third task commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen Return the length of a string
+ * @s: pointer
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int leng = 0;
+
+	while (*s != '\0')
+	{
+		leng++;
+		s++;
+
+	}
+	return (leng);
+}
